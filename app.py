c95515abaa383f896b1fdbda28c6db6eb5cde4ec
alirezabm21 <alirezabaraham97@gmail.com>
2026-08-17 10:59:50 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 From http.server Import BaseHTTPRequesHandler , HTTPServer
 Class Handler(BaseHTTPRequestHandler):
 Def do_GET(self):
-  self.send_response(200)
-  self.send_header("content-type","tet/html")
-  self.end_headers()
-  self.wfile.write(
-  b"<h1>Hello from Docker!</h1>"
-  b"<p>DovOps practice project</p>")
-  server = HTTPServer(("0.0.0.0", 80000),Handler)
-  print("Server running on port 8000")
-  server.server_forever()
+self.send_response(200)
+self.send_header("content-type","tet/html")
+self.end_headers()
+self.wfile.write(
+b"<h1>Hello from Docker!</h1>"
+b"<p>DovOps practice project</p>")
+server = HTTPServer(("0.0.0.0", 80000),Handler)
+print("Server running on port 8000")
+server.server_forever()
